d9c63e8be63dc76799c68027cdb12aa948eb98ac
davidladeji <71533464+davidladeji@users.noreply.github.com>
2025-05-05 16:00:11 -0700

    feat: Created User model for users db

---
+++ src/main/java/com/codewithdave/store/entities/User.java
@@ -0,0 +1,53 @@
+package com.codewithdave.store.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Best practice to use @Column tag to define relationship to db column
+    // This way, these parameter names can change without needing to edit the db
+
+    @Column(nullable = false, name= "name")
+    private String name;
+
+    @Column(nullable = false, name= "email")
+    private String email;
+
+    @Column(nullable = false, name= "password")
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
