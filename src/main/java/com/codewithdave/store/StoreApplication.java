efdc452972aba255a9c8b97a113c1b68893c9cf6
davidladeji <71533464+davidladeji@users.noreply.github.com>
2025-05-06 14:55:07 -0700

    feat: using repositories for crud ops

--- src/main/java/com/codewithdave/store/StoreApplication.java
+++ src/main/java/com/codewithdave/store/StoreApplication.java
@@ -4,12 +4,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.codewithdave.store.entities.User;
+import com.codewithdave.store.repositories.UserRepository;
+
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+		var repository = context.getBean(UserRepository.class);
 
+		// Add user
+		var user = User.builder()
+			.name("John")
+			.email("johnsmith@gmail.com")
+			.password("password")
+			.build();
+
+		repository.save(user);
+
+		// Get User
+		var user2 = repository.findById(1L).orElse(null);
+		// Printing the whole user gives a lazy load error fro some reason
+    	System.out.println(user2.getEmail());
+		
+
+		// Delete User
+		repository.deleteById(1L);
+		
 	}
 
 }
