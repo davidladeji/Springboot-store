5736bdc5ec055d9e651552330dc534d0890fe181
davidladeji <71533464+davidladeji@users.noreply.github.com>
2025-05-08 15:36:22 -0700

    feat: add derived query methods
    These are methods that spring handles the implementation of for the dev.

--- src/main/java/com/codewithdave/store/repositories/ProductRepository.java
+++ src/main/java/com/codewithdave/store/repositories/ProductRepository.java
@@ -1,9 +1,40 @@
 package com.codewithdave.store.repositories;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.codewithdave.store.entities.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
-    
+    // String
+  List<Product> findByName(String name);
+  List<Product> findByNameLike(String name);
+  List<Product> findByNameNotLike(String name);
+  List<Product> findByNameContaining(String name);
+  List<Product> findByNameStartingWith(String name);
+  List<Product> findByNameEndingWith(String name);
+  List<Product> findByNameEndingWithIgnoreCase(String name);
+
+  // Numbers
+  List<Product> findByPrice(BigDecimal price);
+  List<Product> findByPriceGreaterThan(BigDecimal price);
+  List<Product> findByPriceGreaterThanEqual(BigDecimal price);
+  List<Product> findByPriceLessThanEqual(BigDecimal price);
+  List<Product> findByPriceBetween(BigDecimal min, BigDecimal max);
+
+  // Null
+  List<Product> findByDescriptionNull();
+  List<Product> findByDescriptionNotNull();
+
+  // Multiple conditions
+  List<Product> findByDescriptionNullAndNameNull();
+
+  // Sort (OrderBy)
+  List<Product> findByNameOrderByPrice(String name);
+
+  // Limit (Top/First)
+  List<Product> findTop5ByNameOrderByPrice(String name);
+  List<Product> findFirst5ByNameLikeOrderByPrice(String name);
 }
